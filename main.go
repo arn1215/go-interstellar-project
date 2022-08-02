75bf46123d7f0c96d685d550393c7251d19ccbc6
Ali <ali.naqvi.dev@gmail.com>
2022-08-02 15:25:48 -0500

    calculateFuel function

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+func fuelGauge(fuel int) {
+	fmt.Println("Fuel remaining:", fuel)
+}
+
+func calculateFuel(planet string) int {
+	var fuel int
+
+	switch planet {
+	case "Venus":
+		fuel = 300000
+	case "Mercury":
+		fuel = 500000
+	case "Mars":
+		fuel = 700000
+	default:
+		fuel = 0
+	}
+	return fuel
+}
+
 func main() {
+	fmt.Println(calculateFuel("Venus"))
 
 }
